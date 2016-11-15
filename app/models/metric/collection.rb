28d5e364a4e01df4d46b020441e6f5ecb4960e1b
y8 <y8@ya.ru>
2016-11-15 14:14:49 +0100

    Add `nindex` counter metric

--- app/models/metric/collection.rb
+++ app/models/metric/collection.rb
@@ -8,6 +8,7 @@ class Metric
       gauge 'size', as: 'collection_size_bytes'
       gauge 'storageSize', as: 'collection_storage_size_bytes'
       gauge 'avgObjSize', as: 'collection_avg_size_bytes'
+      gauge 'nindexes', as: 'collection_indexes_count'
 
       # Ignore index stats, as we will get them from WT cache info
       ignore 'totalIndexSize', 'indexSizes'
