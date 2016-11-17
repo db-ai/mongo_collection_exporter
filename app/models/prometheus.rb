a58f63f5b1778917aba11340251ffcdbdaaa1f9b
y8 <y8@ya.ru>
2016-11-17 17:43:19 +0100

    Add `scrape_duration_ms` metric

--- app/models/prometheus.rb
+++ app/models/prometheus.rb
@@ -5,20 +5,43 @@ class Prometheus
 
   def initialize(config = Settings.current)
     @config = config
+    @points = []
+
+    prepare!
   end
 
   def to_s
-    lines = all_points_by_name.each_with_object([]) do |(name, points), memo|
+    # Trailing new-line is strictly required by Prometheus
+    @points.join("\n") + "\n"
+  end
+
+  private
+
+  def scrape_duration
+    point = Point::Gauge.new("scrape_duration_ms", (@runtime * 1000.0).to_i)
+
+    [point.to_prom_banner(METRIC_PREFIX), point.to_prom(METRIC_PREFIX)]
+  end
+
+  def prepare!
+    before = Time.now.to_f
+
+    @points = all_points_by_name.each_with_object([]) do |(name, points), memo|
       memo << points.first.to_prom_banner(METRIC_PREFIX)
       memo.concat points.map {|point| point.to_prom(METRIC_PREFIX) }
     end
 
-    # Trailing new-line is strictly required by Prometheus
-    lines.join("\n") + "\n"
+    @runtime = Time.now.to_f - before
+
+    @points.concat scrape_duration
   end
 
   def all_points
     all_metrics.map(&:all).flatten
+  end
+
+  def all_metrics
+    config.mongo.all.map(&:metrics).flatten
   end
 
   def all_points_by_name
@@ -27,9 +50,5 @@ class Prometheus
       memo[full_name] ||= Array.new
       memo[full_name].push(point)
     end
-  end
-
-  def all_metrics
-    config.mongo.all.map(&:metrics).flatten
   end
 end
