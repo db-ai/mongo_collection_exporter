317cb6dfcb358f31e46998f1cd5d58c6fbbafa65
y8 <y8@ya.ru>
2016-11-21 23:12:04 +0100

    Move metrics to App definition

--- app/app.rb
+++ app/app.rb
@@ -4,14 +4,18 @@ module MongoCollectionExporter
     Mongo::Logger.logger.level = Logger::INFO
 
     disable :reload
-
-    register Padrino::Helpers
-
     enable :sessions
     layout :mono
 
     get '/' do
       'mongo_collection_exporter metrics are <a href="/metrics">here</a>'
+    end
+
+    get '/metrics' do
+      content_type 'text/plain;charset=utf8'
+
+      @prometheus = Prometheus.new
+      @prometheus.to_s
     end
   end
 end
--- app/controllers/metrics.rb
+++
@@ -1,8 +0,0 @@
-MongoCollectionExporter::App.controllers :metrics do
-  get :index do
-    content_type 'text/plain;charset=utf8'
-
-    @prometheus = Prometheus.new
-    @prometheus.to_s
-  end
-end
